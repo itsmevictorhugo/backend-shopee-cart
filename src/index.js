63ba7208a97de35de37fda90581434498fff0621
Victor Hugo <csivictorhugo@gmail.com>
2026-03-09 12:45:59 -0300

    feat: logica aplicada do item

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import createItem from './services/item.js';
+
 const cart = [];
+
+console.log('Welcome to the your Shopee Cart!');
+
+const item1 = await createItem('HotWheels Ferrari', 20.99, 1)
+const item2 = await createItem('HotWheels Lamborghini', 39.99, 3)
+
+console.log(item2.subtotal());
