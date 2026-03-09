314ee87e529e098e3db860ba112d709ba408f36a
Victor Hugo <csivictorhugo@gmail.com>
2026-03-09 14:31:53 -0300

    feat: calculo total da compra e lista de desejo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+import * as cartService from './services/cart.js'
 import createItem from './services/item.js';
 
-const cart = [];
+const myCart = [];
+const myWishList = [];
 
 console.log('Welcome to the your Shopee Cart!');
 
 const item1 = await createItem('HotWheels Ferrari', 20.99, 1)
 const item2 = await createItem('HotWheels Lamborghini', 39.99, 3)
 
-console.log(item2.subtotal());
+await cartService.addItem(myCart, item1)
+await cartService.addItem(myWishList, item2)
+
+console.log('Total Shopee cart: ');
+await cartService.calculateTotal(myCart);
--- src/services/cart.js
+++ src/services/cart.js
@@ -2,7 +2,15 @@
 
 // Casos de uso:
 // -> Adicionar item no carrinho
-async function addItem(userCart) {}
+async function addItem(userCart, item) {
+  userCart.push(item);
+}
+
+// -> Calcular o total
+async function calculateTotal(userCart) {
+  const result = userCart.reduce((total, item)=> total + item.subtotal(), 0);
+  console.log(result);
+}
 
 // -> Deletar item do carrinho
 async function deleteItem(userCart, nameItem) {}
@@ -10,5 +18,10 @@ async function deleteItem(userCart, nameItem) {}
 // -> Remover um item - diminui um item
 async function removeItem(userCart, index) {}
 
-// -> Calcular o total
-async function calculateTotal(userCart) {}
+
+export {
+  addItem, 
+  calculateTotal, 
+  deleteItem, 
+  removeItem
+}
