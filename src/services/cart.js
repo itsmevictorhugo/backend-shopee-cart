d422b0e61d875ae3e0afffa80969e8bf154721af
Victor Hugo <csivictorhugo@gmail.com>
2026-03-09 12:28:59 -0300

    feat: assinaturas do arquivo cart

---
+++ src/services/cart.js
@@ -0,0 +1,14 @@
+// quais ações meu carrinho pode fazer
+
+// Casos de uso:
+// -> Adicionar item no carrinho
+async function addItem(userCart) {}
+
+// -> Deletar item do carrinho
+async function deleteItem(userCart, nameItem) {}
+
+// -> Remover um item - diminui um item
+async function removeItem(userCart, index) {}
+
+// -> Calcular o total
+async function calculateTotal(userCart) {}
